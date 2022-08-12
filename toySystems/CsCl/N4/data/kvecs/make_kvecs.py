039c229ca232c1e2bdc820f12b4a35529652c0ad
bernetmeijer <b.e.meijer@qmul.ac.uk>
2022-08-12 17:37:26 +0100

    put data into N4 directory

--- toySystems/CsCl/N4/data/kvecs/make_kvecs.py
+++ toySystems/CsCl/N4/data/kvecs/make_kvecs.py
@@ -4,5 +4,5 @@ import numpy as np
 # just simple kvecs
 k_vecs = np.array([[0.0, 0.0, 0.0], [0.25, 0.0, 0.0], [0.5, 0.0, 0.0]])
 
-with open('./kvecs_N4_GX.pkl', 'wb') as handle:
+with open('kvecs_N4_GX.pkl', 'wb') as handle:
     pickle.dump(k_vecs, handle)
